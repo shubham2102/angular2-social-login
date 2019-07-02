efab3a8fdae798825e2afeb102abbcf6b91f0a7f
shubham2102 <shubham2102@gmail.com>
2019-07-02 15:35:52 +0530

    changes for supporting this module with angular ssr

--- dist/angular2-social-login.module.js
+++ dist/angular2-social-login.module.js
@@ -10,51 +10,54 @@ var Angular2SocialLoginModule = (function () {
     function Angular2SocialLoginModule() {
     }
     Angular2SocialLoginModule.loadProvidersScripts = function (config) {
-        var loadProvidersScripts = {
-            google: function (info) {
-                var d = document, gJs, ref = d.getElementsByTagName('script')[0];
-                gJs = d.createElement('script');
-                gJs.async = true;
-                gJs.src = "//apis.google.com/js/platform.js";
-                gJs.onload = function () {
-                    gapi.load('auth2', function () {
-                        gapi.auth2.init({
-                            client_id: info["clientId"],
-                            scope: 'email'
+        if(typeof window != 'undefined'){
+            var loadProvidersScripts = {
+                google: function (info) {
+                    var d = document, gJs, ref = d.getElementsByTagName('script')[0];
+                    gJs = d.createElement('script');
+                    gJs.async = true;
+                    gJs.src = "//apis.google.com/js/platform.js";
+                    gJs.onload = function () {
+                        gapi.load('auth2', function () {
+                            gapi.auth2.init({
+                                client_id: info["clientId"],
+                                scope: 'email'
+                            });
                         });
-                    });
-                };
-                ref.parentNode.insertBefore(gJs, ref);
-            },
-            linkedin: function (info) {
-                var lIN, d = document, ref = d.getElementsByTagName('script')[0];
-                lIN = d.createElement('script');
-                lIN.async = false;
-                lIN.src = "//platform.linkedin.com/in.js";
-                lIN.text = ("api_key: " + info["clientId"]).replace("\"", "");
-                ref.parentNode.insertBefore(lIN, ref);
-            },
-            facebook: function (info) {
-                var d = document, fbJs, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-                fbJs = d.createElement('script');
-                fbJs.id = id;
-                fbJs.async = true;
-                fbJs.src = "//connect.facebook.net/en_US/sdk.js";
-                fbJs.onload = function () {
-                    FB.init({
-                        appId: info["clientId"],
-                        status: true,
-                        cookie: true,
-                        xfbml: true,
-                        version: info["apiVersion"]
-                    });
-                };
-                ref.parentNode.insertBefore(fbJs, ref);
-            }
-        };
-        Object.keys(config).forEach(function (provider) {
-            loadProvidersScripts[provider](config[provider]);
-        });
+                    };
+                    ref.parentNode.insertBefore(gJs, ref);
+                },
+                linkedin: function (info) {
+                    var lIN, d = document, ref = d.getElementsByTagName('script')[0];
+                    lIN = d.createElement('script');
+                    lIN.async = false;
+                    lIN.src = "//platform.linkedin.com/in.js";
+                    lIN.text = ("api_key: " + info["clientId"]).replace("\"", "");
+                    ref.parentNode.insertBefore(lIN, ref);
+                },
+                facebook: function (info) {
+                    var d = document, fbJs, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+                    fbJs = d.createElement('script');
+                    fbJs.id = id;
+                    fbJs.async = true;
+                    fbJs.src = "//connect.facebook.net/en_US/sdk.js";
+                    fbJs.onload = function () {
+                        FB.init({
+                            appId: info["clientId"],
+                            status: true,
+                            cookie: true,
+                            xfbml: true,
+                            version: info["apiVersion"]
+                        });
+                    };
+                    ref.parentNode.insertBefore(fbJs, ref);
+                }
+            };
+            Object.keys(config).forEach(function (provider) {
+                loadProvidersScripts[provider](config[provider]);
+            });
+        }
+
     };
     Angular2SocialLoginModule = __decorate([
         NgModule({
